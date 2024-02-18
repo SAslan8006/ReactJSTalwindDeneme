937207a2527a10329485fd7f3331ad24f20ce5cc
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-02-18 14:39:51 +0300

    Animasyon 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Box2 from "../src/components/Box2"
 import Box3 from './components/Box3';
 import Box4 from "../src/components/Box4"
 import Box5 from "./components/Box5";
+import Box6 from "./components/Box6";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Box3 />
         <Box4 />
         <Box5 />
+        <Box6 />
 
       </div>
       <div className="min-h-screen bg-[#060000]">
---
+++ src/components/Box6.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { motion, useAnimation } from 'framer-motion';
+import styles from './../Styles';
+
+const Box6 = () => {
+    const control = useAnimation();
+
+    return (
+        <div className='bg-[#191825] m-0 p-5'>
+            <button onClick={() => {
+                control.start({
+                    x: 100,
+                    transition: { duration: 2 }
+
+                })
+            }} className={styles.button2}>Right</button>
+            <button onClick={() => {
+                control.start({
+                    x: -100,
+                    transition: { duration: 2 }
+
+                })
+            }} className={styles.button2}>Left</button>
+            <button onClick={() => {
+                control.start({
+                    borderRadius: '20%',
+                    transition: { duration: 2 }
+
+                })
+            }} className={styles.button2}>Circle</button>
+            <button onClick={() => {
+                control.start({
+                    borderRadius: '0%',
+                    transition: { duration: 2 }
+
+                })
+            }} className={styles.button2}>Square</button>
+            <motion.div className={styles.animasyonBox}
+                animate={control}
+            >
+            </motion.div>
+        </div >
+    )
+}
+
+export default Box6
